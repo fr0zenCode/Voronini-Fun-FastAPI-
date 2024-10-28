8f2087f41a9d50e7e83ec85cf43004c162db4bb1
Slava <slavik3001@rambler.ru>
2024-10-28 18:03:53 +0300

    Refactor commit: deleted functions for user api, because they are in "user" package now

--- main.py
+++ main.py
@@ -1,52 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, Request, Form
-from pydantic import BaseModel
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
+from modules.user.api import user_router
 
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
-
-
-class Post(BaseModel):
-    author: str
-    text: str
-
-
-@app.get("/")
-def foo_get(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
-
-
-@app.post("/get-post")
-def foo(request: Request, author: str = Form(...), text: str = Form(...)):
-    return templates.TemplateResponse(request=request, name="post.html", context={"author": author, "text": text})
-
-
-@app.get("/feed")
-def get_feed():
-    ...
-
-
-@app.get("/authorization")
-def authorization_page():
-    ...
-
-
-@app.get("/user-cabinet")
-def user_cabinet():
-    ...
-
-
-@app.get("/secret-page")
-def casino_page():
-    ...
-
-
-@app.get("detail-seria-info")
-def detail_seria_info():
-    ...
+app.include_router(user_router, prefix="/user")
 
 
 if __name__ == "__main__":
