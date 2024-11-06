923e9a568a5d9a3a60911532a835a80597607789
Slava <slavik3001@rambler.ru>
2024-11-06 13:34:56 +0300

    Mounted the path for the static files

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from modules.user.api import user_router
 
+static_folder = "static"
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=static_folder), name="static")
+
 app.include_router(user_router, prefix="/user")
 
 
