2c3323ab92ddefc3637a21d033db87bec86c887e
Slava <slavik3001@rambler.ru>
2024-11-07 09:03:11 +0300

    Added params for new TestDB.

--- config.py
+++ config.py
@@ -21,11 +21,25 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
     @property
     def database_url_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    @property
+    def test_database_url_asyncpg(self):
+        return (f"postgresql+asyncpg://{self.TEST_DB_USER}:"
+                f"{self.TEST_DB_PASS}@"
+                f"{self.TEST_DB_HOST}:"
+                f"{self.TEST_DB_PORT}/"
+                f"{self.TEST_DB_NAME}")
+
+    model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
     auth_jwt: AuthJWT = AuthJWT()
 
 
