31ae0cb65030d7af0ed1295598949ee5d13fafbe
Slava <100254170+fr0zenCode@users.noreply.github.com>
2025-01-08 04:31:39 +0300

    Delete static directory

--- static/js/base-script.js
+++
@@ -1,7 +0,0 @@
-async function logout(){
-    const response = await fetch("/user/logout", {
-        method: "POST",
-        headers: { "Accept": "application/json", "Content-Type": "application/json" }
-        })
-    };
-    
