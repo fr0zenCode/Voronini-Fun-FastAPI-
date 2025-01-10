68892e56ef2ab371ca7947f4311e3cea0e12e646
Slava <slavik3001@rambler.ru>
2025-01-10 14:57:35 +0300

    Added some type hints

--- app/api/user/api.py
+++ app/api/user/api.py
@@ -18,7 +18,7 @@ async def user_cabinet(
         response: Response,
         request: Request,
         auth_service: Annotated[AuthService, Depends(auth_service_factory)]
-):
+) -> dict:
 
     access_token = await auth_service.is_user_authorized(access_token=request.cookies.get("access-token"),
                                                          refresh_token=request.cookies.get("refresh-token"))
@@ -26,6 +26,7 @@ async def user_cabinet(
     response.set_cookie(key=auth_service.ACCESS_TOKEN_COOKIES_ALIAS, value=access_token)
 
     user_info = await auth_service.convert_jwt_to_read_format(jwt=access_token)
+
     return user_info
 
 
@@ -33,7 +34,7 @@ async def user_cabinet(
 async def registrate_user(
         user_for_registrate: UserAddSchema,
         users_service: Annotated[UsersService, Depends(users_service_factory)]
-):
+) -> dict:
     new_user_id = await users_service.registrate_user(user_for_registrate)
     return {"message": f"successfully. Your ID in system is {new_user_id}"}
 
@@ -43,12 +44,13 @@ async def authenticate_user(
         response: Response,
         users_credentials: UsersCredentialsForLogin,
         users_service: Annotated[UsersService, Depends(users_service_factory)]
-):
+) -> dict:
     await users_service.authenticate_user(
         email=users_credentials.email,
         password=users_credentials.password,
         response=response
     )
+    return {"message": "successful"}
 
 
 @user_router.post("/logout")
@@ -56,5 +58,6 @@ async def logout(
         response: Response,
         request: Request,
         users_service: Annotated[UsersService, Depends(users_service_factory)]
-):
+) -> dict:
     await users_service.logout(request=request, response=response)
+    return {"message": "successful"}
