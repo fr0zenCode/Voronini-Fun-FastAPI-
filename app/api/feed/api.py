fdaf52bfc5d45f082c3cef8fa0fdc7f245a7d9a0
Slava <slavik3001@rambler.ru>
2025-01-10 15:02:47 +0300

    Created feed api

--- app/api/feed/api.py
+++ app/api/feed/api.py
@@ -1,33 +1,19 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Request, Depends
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, Depends
 
-from services.auth.auth import AuthService, auth_service_factory
+from database.posts.schemas import PostSchema
+from services.feed.abstract import AbstractFeedService
+from services.feed.feed import feed_service_factory
 
-feed_router = APIRouter(prefix="/feed", tags=["feed"])
-
-templates = Jinja2Templates(directory="templates")
+feed_router = APIRouter(prefix="/feed", tags=["Feed API"])
 
 
-# @feed_router.get("/")
-# async def get_feed_page(request: Request, auth_service: Annotated[AuthService, Depends(get_auth_service)]):
-#     decoded_jwt = await auth_service.is_user_authorized(request=request)
-#     return frontend_service.created_feed_template(decoded_jwt=decoded_jwt)
-#
-#
-# @feed_router.get("/load-more-posts")
-# async def load_more_posts(offset: int, limit: int):
-#     await posts_service.get_more_posts(offset=offset, limit=limit)
-#
-#
-# @feed_router.post("/new-post")
-# async def add_new_post(post_for_add: AddPostSchema, request: Request):
-#     decoded_jwt = await auth_service.is_user_authorized(request=request)
-#     await posts_service.add_post(decoded_jwt=decoded_jwt, post_for_add=post_for_add)
-#
-#
-# @feed_router.post("/delete-post")
-# async def delete_post(post_id: int, request: Request):
-#     decoded_jwt = await auth_service.is_user_authorized(request=request)
-#     await posts_service.delete_post(post_id=post_id)
+@feed_router.get("/get-posts-portion")
+async def get_posts_portion(
+        feed_service: Annotated[AbstractFeedService, Depends(feed_service_factory)],
+        limit: int = 10,
+        offset: int = 0
+) -> list[PostSchema]:
+    posts = await feed_service.get_posts_portion(limit=limit, offset=offset)
+    return posts
