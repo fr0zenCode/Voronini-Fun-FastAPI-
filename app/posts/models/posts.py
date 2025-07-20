a9328b441a392847d5a81caf051d21ac46da4468
Slava <slavik3001@rambler.ru>
2025-07-21 01:44:45 +0300

    fix: fix magic numbers, create constants; fix __table_args__ type; fix PatchSchema type

--- app/posts/models/posts.py
+++ app/posts/models/posts.py
@@ -5,6 +5,10 @@ from sqlalchemy import Index, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.core.database import Base
+from common.config import Base
+
+POST_HEADER_MAX_LENGTH = 100
+POST_TEXT_CONTENT_MAX_LENGTH = 356
 
 
 class Posts(Base):
@@ -16,10 +20,12 @@ class Posts(Base):
     header: Mapped[str] = mapped_column(String(100)) # TODO: delete magic number
     text_content: Mapped[str] = mapped_column(String(356)) # TODO: delete magic number
 
+    header: Mapped[str] = mapped_column(String(POST_HEADER_MAX_LENGTH))
+    text_content: Mapped[str] = mapped_column(String(POST_TEXT_CONTENT_MAX_LENGTH))
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
-    __table_args__ = (Index("author_id_hash_index", author_id, postgresql_using="hash"))
+    __table_args__ = (Index("author_id_hash_index", author_id, postgresql_using="hash"), )
 
     InputSchema = MappedModel.create(columns=[header, text_content])
-    PatchSchema = InputSchema.extend(columns=[created_at])
-    ResponseSchema = MappedModel.create(columns=[id, author_id, header, text_content, created_at])
+    PatchSchema = InputSchema.extend(columns=[created_at]).as_patch()
+    ResponseSchema = InputSchema.extend(columns=[id, author_id, created_at])
