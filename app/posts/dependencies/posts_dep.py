4c27aff033f72590ba4e105f3617e77ace250860
Slava <slavik3001@rambler.ru>
2025-08-14 00:25:24 +0300

    refactor: add aliases

--- app/posts/dependencies/posts_dep.py
+++ app/posts/dependencies/posts_dep.py
@@ -1,15 +1,16 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from starlette import status
 
 from posts.models.posts import Posts
+from users.aliases import POST_ID_NON_EXIST_ALIAS
 
 
 async def get_post_by_id(post_id: int) -> Posts:
     post = await Posts.find_first_by_id(post_id)
     if not post:
-        raise status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=POST_ID_NON_EXIST_ALIAS)
     return post
 
 PostById = Annotated[Posts, Depends(get_post_by_id)]
