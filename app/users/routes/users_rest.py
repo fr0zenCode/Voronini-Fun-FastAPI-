b3d4e83d38aae92e74799af348164eee55414b9c
Slava <slavik3001@rambler.ru>
2025-08-14 00:15:25 +0300

    feat: add function for creating user + change prefixes

--- app/users/routes/users_rest.py
+++ app/users/routes/users_rest.py
@@ -3,22 +3,22 @@ from starlette import status
 
 from users.dependencies.users_dep import GetCurrentAuthorizedUser
 from users.models.users_db import Users
+from users.utils.users_utils import create_user_util
 
-
-user_router = APIRouter(prefix="/users", tags=["User"])
+user_router = APIRouter(prefix="/users-api", tags=["User"])
 
 @user_router.post(
-    path="/user/",
+    path="/users/",
     status_code=status.HTTP_201_CREATED,
     response_model=Users.ResponseSchema,
     summary="Create a new user"
 )
 async def create_user(user_data: Users.InputSchema) -> Users:
-    return await Users.create(**user_data.model_dump())
+    return await create_user_util(user_data)
 
 
 @user_router.get(
-    path="/user/",
+    path="/users/user/",
     summary="Get current authenticated user"
 )
 async def get_current_user(user: GetCurrentAuthorizedUser):
