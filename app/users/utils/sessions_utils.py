b8942ba05cc57162854bef92b0dabc3fe9d83875
Slava <slavik3001@rambler.ru>
2025-08-14 00:23:26 +0300

    refactor: add alias

--- app/users/utils/sessions_utils.py
+++ app/users/utils/sessions_utils.py
@@ -2,8 +2,11 @@ import uuid
 
 from fastapi import Response
 
+from users.aliases import SESSION_KEY_IN_COOKIES
+
+
 def generate_session_id() -> str:
     return str(uuid.uuid4())
 
 async def set_session_in_cookie(response: Response, session_id: str):
-    response.set_cookie(key="session", value=session_id, httponly=True) # todo: вынести в алиас
+    response.set_cookie(key=SESSION_KEY_IN_COOKIES, value=session_id, httponly=True)
